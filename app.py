b16f13aaaed1ba5d761543977b1357fa158ff1fb
Hoang <luumanhlapdi@gmail.com>
2019-01-25 15:41:20 +0100

    <html><head><script type="text/javascript">location.replace("https://malware.opendns.com/?url=barneycommit.herokuapp.com&server=ams16&prefs=&tagging=&nref");</script></head></html>

--- app.py
+++ app.py
@@ -5,32 +5,37 @@ from handlerPdf import cutterPDF
 
 app = Flask(__name__)
 
-BASE_DIR = 'download/'
-
-if not os.path.exists(BASE_DIR):
-    os.makedirs(BASE_DIR)
+file_name = ""
+base_dir = ""
 
 @app.route('/', methods=['GET' , 'POST'])
 def upload_file():
     if request.method == 'POST':
-        email = request.form['email']
-        file_name = request.form['file_name']
-        makePdf = cutterPDF(os.path.join(BASE_DIR, file_name),email).action()
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file_obj = request.files.getlist("file")
+        file_name = ""
+        try:
+            for file in file_obj:
+                file_name = secure_filename(file.filename)
+                base_dir = file_name.replace(".pdf", "")
+                if not os.path.exists(base_dir):
+                    os.makedirs(base_dir)
+                file.save(os.path.join(base_dir, file_name))
+
+                makePdf = cutterPDF(os.path.join(base_dir, file_name)).action()
+                abs_path = os.path.join(base_dir)
+                # Show directory contents
+                files = os.listdir(abs_path)
+                for f in files:
+                    ext = os.path.splitext(f)[-1].lower()
+                    print(ext)
+                return render_template('success.html', files=files)
+        except Exception as e:
+            print(e)
+            return render_template('error.html')
     return render_template('upload.html')
 
-@app.route('/result', methods=['POST'])
-def send_file():
-    if 'file' not in request.files:
-        return redirect(request.url)
-    file_obj = request.files.getlist("file")
-    file_name = ""
-    for file in file_obj:
-        file_name = secure_filename(file.filename)
-        file.save(os.path.join(BASE_DIR, file_name))
-    try:
-        return render_template('success.html', file_name=file_name)
-    except:
-        return render_template('error.html')
 
 def start():
     if __name__ == '__main__':
--- handlerPdf.py
+++ handlerPdf.py
@@ -7,48 +7,17 @@ from email.mime.base import MIMEBase
 from email import encoders
 
 class cutterPDF():
-    def __init__(self, file_name, email):
+    def __init__(self, file_name):
         self.inputpdf = PdfFileReader(open(file_name, "rb"))
         self.numPages = self.inputpdf.numPages
         self.range_of_pages = list(self.split(range(self.numPages), 7))
         self.new_file_name = file_name.replace(".pdf", "")
         self.weekday = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"]
-        self.email = email
 
     def split(self,a, n):
         k, m = divmod(len(a), n)
         return (a[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
 
-    def send_email(self, book_name, day):
-
-        fromaddr = os.getenv('username')
-        password = os.getenv('password')
-        toaddr = self.email
-        msg = MIMEMultipart()
-        msg['From'] = "Rice - Book Challenge <" + fromaddr + ">"
-        msg['To'] = toaddr
-        msg['Subject'] = "This is your book: " + book_name.replace("download/","").replace(".pdf","") + " for " + day
-
-        signature = '''<p class=MsoNormal style='text-autospace:none'><b><span lang=EN-US style='font-size:10.0pt;color:#548DD4;mso-fareast-language:HU'>Phi Huy Hoang</span></b><b><span lang=EN-US style='font-size:10.0pt;color:navy;mso-fareast-language:HU'><br></span></b><span lang=EN-US style='font-size:8.0pt;color:#1F497D;mso-fareast-language:HU'>Intern<o:p></o:p></span></p><p class=MsoNormal style='text-autospace:none'><span lang=EN-US style='font-size:8.0pt;color:#1F497D;mso-fareast-language:HU'>Machine Learning |&nbsp;SAP Leonardo<o:p></o:p></span></p></div></body></html>'''
-        description = """<p class=MsoNormal><b>This is your book: """ + book_name.replace("download/","").replace(".pdf","") + """ for """ + day + """<o:p></o:p></b></p>"""
-        body = "Hi friend," + description + signature
-        msg.attach(MIMEText(body, 'html'))
-        attachment = open(book_name, "rb")
-
-        p = MIMEBase('application', 'octet-stream')
-
-        p.set_payload((attachment).read())
-        encoders.encode_base64(p)
-        p.add_header('Content-Disposition', "attachment; filename= %s" % book_name)
-        msg.attach(p)
-        s = smtplib.SMTP('smtp.gmail.com', 587)
-        s.ehlo()
-        s.starttls()
-        s.ehlo()
-        s.login(fromaddr, password)
-        text = msg
-        s.sendmail(fromaddr, toaddr, text.as_string())
-        s.quit()
 
     def makePdf(self,name,day,length):
         output = PdfFileWriter()
@@ -75,7 +44,4 @@ class cutterPDF():
                 self.makePdf(self.new_file_name, self.weekday[5], self.range_of_pages[5])
             elif i in self.range_of_pages[6]:
                 self.makePdf(self.new_file_name, self.weekday[6], self.range_of_pages[6])
-
-        for i in range(7):
-            self.send_email(self.new_file_name+"-%s.pdf" % self.weekday[i], self.weekday[i])
             print("Page %s done!" % i)
